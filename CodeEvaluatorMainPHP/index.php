a6d076c5ef14c32f92d0c76803239a2419aded8b
Devashish Shankar <devashish.shankar@gmail.com>
2012-07-26 18:10:32 +0530

    Added webpage folder
    This folder is the main folder for netbeans project. Import the project
    into netbeans from here and also copy it to the www folder in wamp
    server

---
+++ CodeEvaluatorMainPHP/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        is it working?
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
